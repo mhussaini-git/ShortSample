1fe72a5b259ca54958299aa729cfc30446065266
mhussaini-git <61485561+mhussaini-git@users.noreply.github.com>
2020-03-24 13:57:40 -0400

    Add files via upload

---
+++ Pulling SQL Queries.py
@@ -0,0 +1,35 @@
+#Pulling SQL Queries to load them to target database. 
+
+sqlserver_extract = ('''
+  SELECT sqlserver_column_1, sqlserver_column_2, sqlserver_column_3
+  FROM sqlserver_table
+''')
+
+sqlserver_insert = ('''
+  INSERT INTO table (column_1, column_2, column_3)
+  VALUES (?, ?, ?)  
+''')
+
+mysql_extract = ('''
+  SELECT mysql_column_1, mysql_column_2, mysql_column_3
+  FROM mysql_table
+''')
+
+mysql_insert = ('''
+  INSERT INTO table (column_1, column_2, column_3)
+  VALUES (?, ?, ?)  
+''')
+
+# exporting queries
+class SqlQuery:
+  def __init__(self, extract_query, load_query):
+    self.extract_query = extract_query
+    self.load_query = load_query
+    
+# create instances for SqlQuery class
+sqlserver_query = SqlQuery(sqlserver_extract, sqlserver_insert)
+mysql_query = SqlQuery(mysql_extract, mysql_insert)
+
+# store as list for iteration
+sqlserver_queries = [sqlserver_query]
+mysql_queries = [mysql_query]
